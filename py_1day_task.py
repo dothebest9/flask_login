eba59a242adfb1a87dffd79cfe99d3463e8d7129
dothebest9 <smrmtjsrn11@naver.com>
2025-07-09 14:29:57 +0900

    py_1day_task

---
+++ py_1day_task.py
@@ -0,0 +1,24 @@
+#quiz 1
+sum1=14+29
+print(sum1)
+#quiz 2
+print(68-50)
+#quiz 3
+sum2=sum1*4
+print(sum2)
+#quiz 4
+sum3=sum2//4
+print(sum3)
+#quiz 5
+print(500+sum1)
+#quiz 6
+print(sum1**0.5)
+#quiz 7
+sum4= 60/(20/5)
+print(sum4/3600)
+#quiz 8
+sum5=0.5*9.8*((sum4+3)**2)
+print(sum5)
+#quiz last
+sum6=32-128
+print(abs(sum6))
